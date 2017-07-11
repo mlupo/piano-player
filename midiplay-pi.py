410c79e34be2ca5ce5e8f2977970da6b31197a3a
Max Lupo <art@maxlupo.com>
2017-07-10 21:17:30 -0400

    midiplay.py is the file as used in the Beep-Boopatronics thesis project

---
+++ midiplay-pi.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+
+import mido
+import Adafruit_PCA9685
+
+inport = mido.open_input('CH345 MIDI 1')
+
+# Initialise the PCA9685 using the default address (0x40).
+pwm = Adafruit_PCA9685.PCA9685()
+
+
+# Set frequency to 60hz, good for servos.
+pwm.set_pwm_freq(60)
+
+# Configure min and max servo pulse lengths
+servo_min = 140  # Min pulse length out of 4096
+
+servo_max = 600  # Max pulse length out of 4096
+servo_press = 310
+servo_low_press = 270
+
+ON = servo_press
+OFF = servo_min
+
+C4 = 60
+D4 = 62
+Eb4 = 63
+E4 = 64
+F4 = 65
+G4 = 67
+A4 = 69
+B4 = 71
+C5 = 72
+D5 = 74
+E5 = 76
+F5 = 77
+G5 = 79
+A5 = 81
+B5 = 83
+C6 = 84
+
+NOTE_DICT = {C4: 0, D4: 1, E4: 2, F4: 3, G4: 4, A4: 5, B4: 6,
+             C5: 7, D5: 8, E5: 9, F5: 10, G5: 11, A5: 12, B5: 13,
+             C6:14, Eb4 : 15}
+
+def noteCheck(in_message, pwm_channel=0):
+    """take the data attributes of the midi event, and assign it
+    to the appropiate values to work with the servos. If the data is not
+    in the dictionary of notes, pass it along"""
+    note_servo = None
+    signal= None
+    if in_message.type == 'note_on':
+        if in_message.note in NOTE_DICT.keys():
+            note_servo = NOTE_DICT.get(in_message.note)
+            if in_message.velocity > 0:
+                signal = ON
+    elif in_message.type == 'note_off' or in_message.velocity == 0:
+        if in_message.note in NOTE_DICT.keys():
+                note_servo = NOTE_DICT.get(in_message.note)
+                signal = OFF
+    elif in_message.type == 'control_change':
+        print("all notes off")
+        for i in range(0, 14):
+            pwm.set_pwm(i, pwm_channel, OFF)
+    else:
+        return in_message
+
+    if (note_servo is not None) and (signal is not None):
+        pwm.set_pwm(note_servo, pwm_channel, signal)
+        print(note_servo, signal)
+
+
+while True:
+    with inport as port:
+        for message in port:
+            noteCheck(message)
--- midiplay.py
+++ midiplay.py
@@ -1,31 +1,28 @@
 #!/usr/bin/python3
-
-from __future__ import division
-#import sys
-#import time
-import mido
-import Adafruit_PCA9685
-
-inport = mido.open_input('CH345 MIDI 1')
-
-# Initialise the PCA9685 using the default address (0x40).
-pwm = Adafruit_PCA9685.PCA9685()
-
-# Alternatively specify a different address and/or bus:
-# pwm = Adafruit_PCA9685.PCA9685(address=0x41, busnum=2)
-
-# Set frequency to 60hz, good for servos.
-pwm.set_pwm_freq(60)
+import pca9685
+import servo
+import machine
+from midi.midiin import MidiIn
 
 # Configure min and max servo pulse lengths
-servo_min = 155  # Min pulse length out of 4096
+servo_min = 140  # Min pulse length out of 4096
 
 servo_max = 600  # Max pulse length out of 4096
-servo_press = 278
+servo_press = 310
+servo_low_press = 270
 
 ON = servo_press
+ON_LOW = servo_low_press
 OFF = servo_min
 
+C4 = 60
+D4 = 62
+Eb4 = 63
+E4 = 64
+F4 = 65
+G4 = 67
+A4 = 69
+B4 = 71
 C5 = 72
 D5 = 74
 E5 = 76
@@ -34,44 +31,65 @@ G5 = 79
 A5 = 81
 B5 = 83
 C6 = 84
-D6 = 86
-E6 = 88
-F6 = 89
-G6 = 91
-A6 = 93
-B6 = 95
 
-NOTE_DICT = {C5: 0, D5: 1, E5: 2, F5: 3, G5: 4, A5: 5, B5: 6,
-             C6: 7, D6: 8, E6: 9, F6: 10, G6: 11, A6: 12, B6: 13}
+NOTE_DICT = {C4: 0, D4: 1, E4: 2, F4: 3, G4: 4, A4: 5, B4: 6,
+             C5: 7, D5: 8, E5: 9, F5: 10, G5: 11, A5: 12, B5: 13,
+             C6:14, Eb4 : 15}
 
-def noteCheck(in_message, pwm_channel=0):
-    note_servo = None
-    signal= None
-    """take the data attributes of the midi event, and assign it
+def midi_printer(msg):
+    """take the data of the midi event, and assign it
     to the appropiate values to work with the servos. If the data is not
     in the dictionary of notes, pass it along"""
-    if in_message.type != 'control_change':
-        if in_message.type == 'note_on':
-            if in_message.note in NOTE_DICT.keys():
-                note_servo = NOTE_DICT.get(in_message.note)
-                if in_message.velocity > 0:
-                    signal = ON
-        elif in_message.type == 'note_off':
-            if in_message.note in NOTE_DICT.keys():
-                note_servo = NOTE_DICT.get(in_message.note)
+    note_servo = None
+    signal= None
+    #print(tuple(msg))
+    if msg[0] == 144 or msg[0] == 128:
+    # '123' is the 'all sound off' message. if reieved, we cut the noise
+        if msg[0] == 144:
+            if msg[1] in NOTE_DICT.keys():
+                note_servo = NOTE_DICT.get(msg[1])
+                if msg[2] > 0:
+                # note velocity
+                    if msg[1] <= 72:
+                        signal = ON_LOW
+                    else:
+                        signal = ON
+                elif msg[2] == 0:
+                    # if there is a note_on with 0 velocity, turn it off
+                    signal = OFF
+        elif msg[0] == 128:
+            if msg[1] in NOTE_DICT.keys():
+                note_servo = NOTE_DICT.get(msg[1])
                 signal = OFF
-    elif in_message.type == 'control_change':
+    elif msg[0] == 176:
         print("all notes off")
+        print("status off", msg[0])
+        for i in range(0, 13):
+            servos.position(i, duty=OFF)
+            pyb.delay(10)
     else:
-        return in_message
+        print("status", msg[0])
 
     if (note_servo is not None) and (signal is not None):
-        pwm.set_pwm(note_servo, pwm_channel, signal)
+        servos.position(note_servo, duty=signal)
         print(note_servo, signal)
 
+uart = pyb.UART(1, 31250)
+midiin = MidiIn(uart, callback=midi_printer)
+### main prog###
+
+#while True:
+#    data = midiin.poll()
+#    pyb.udelay(50)
+
+i2c = machine.I2C(machine.Pin('B10'), machine.Pin('B3'))
+#i2c.init()
+servos = servo.Servos(i2c)
 
 while True:
-    with inport as port:
-        for message in port:
-            noteCheck(message)
+    midiin.poll()
+    pyb.udelay(20)
+
+#i2c.deinit()
+
 
